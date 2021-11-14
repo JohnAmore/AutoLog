66cbeb635d0c630be033ae21eeaaccfe7c74137b
ItzTaco <danben3@hotmail.com>
2021-11-14 02:42:13 -0500

    Added Comments, removed unnecessary imports.

--- classes/jsonConvert.py
+++ classes/jsonConvert.py
@@ -7,20 +7,23 @@
 # This class is designed to convert the .json file into a Python Dictionary, and send the data into
 # the main file when called to be used as a variable for the information.
 # This will have a connection to the web driving class.
+
+# IMPORTS
 from json import *
 
 
 class jsonConvert(object):
 
     def __init__(self):
-        self.login = r"json/loginInfo.json"
+        self.login = r"json/loginInfo.json"  # Sets JSON file location
 
+    # loadJson opens and returns the JSON data through a Python Dictionary.
     def loadJson(self):
         loginInfo = load(open(self.login))
         return loginInfo
 
+    # dumpJson takes the Python Dictionary and turns it back into JSON, writes it to the file, and then closes it. "Saving"
     def dumpJson(self, dict):
         with open(self.login, "w") as f:
-
             f.write(dumps(dict))
             f.close()
--- classes/webDrive.py
+++ classes/webDrive.py
@@ -2,35 +2,33 @@
 # Name: Daniel Bennett
 # ID: 0593616
 # E-mail: ddbennett@cn.edu
-import time
 
+# IMPORTS
 from selenium import webdriver
-from selenium import *
-from os import *
 from selenium.webdriver.chrome.options import *
-
-
-# webDrive
-# This class is designed to open Chrome and use the login information returned from jsonConvert.py
-# to login on select websites.
 from selenium.webdriver.common.by import By
 
+
+# Class webDrive
+# This class is designed to open Chrome and use the login information returned from jsonConvert.py
+# to login on select websites.
 
 class webDrive(object):
     def __init__(self):
         print('')
 
+    # LOGIN FUNCTION
     def login(self, url, user, password, userHTML, passHTML, buttonHTML):
         chrome_options = Options()
-        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_experimental_option("detach",
+                                               True)  # This and the line above allows the Chrome browser to detach from the program and become its own entity.
         driver = webdriver.Chrome(r"driver/chromedriver", chrome_options=chrome_options)  # Web Driver Instantiation
         driver.get(url)  # Link for website
-        username_tag = driver.find_element(By.XPATH, userHTML)
-        pass_tag = driver.find_element(By.XPATH, passHTML)
-        button_tag = driver.find_element(By.XPATH, buttonHTML)
+        username_tag = driver.find_element(By.XPATH, userHTML)  # HTML ELEMENT FOR USERNAME FIELD
+        pass_tag = driver.find_element(By.XPATH, passHTML)  # HTML ELEMENT FOR PASSWORD FIELD
+        button_tag = driver.find_element(By.XPATH, buttonHTML)  # HTML ELEMENT FOR LOGIN BUTTON
         # These will enter the fields and click the sign in button.
         username_tag.send_keys(user)
         pass_tag.send_keys(password)
 
         button_tag.submit()
-
